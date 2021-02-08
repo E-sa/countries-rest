a139720d57d74bca35c97c4be98d5668cd470643
E-sa <35602280+E-sa@users.noreply.github.com>
2021-02-08 22:33:46 +0330

    Update FilterByRegion.js

--- src/component/FilterByRegion.js
+++ src/component/FilterByRegion.js
@@ -5,10 +5,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import '../style/style.scss';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
-function filterByRegion () {
+function filterByRegion ({onClickFilterRegion}) {
 
     const filterByRegion = 
      <div className="container" id="filter" >
@@ -18,11 +18,12 @@ function filterByRegion () {
         Filter by Region
         </button>
         <div className="dropdown-menu">
-        <a className="dropdown-item" href="Africa">Africa</a>
-        <a className="dropdown-item" href="/Americas">America</a>
-        <a className="dropdown-item" href="/Asia">Asia</a>
-        <a className="dropdown-item" href="/Europe">Europe</a>
-        <a className="dropdown-item" href="/Oceania">Oceania</a>
+        <Link className="dropdown-item" to="/" onClick={()=>onClickFilterRegion("all")} >All</Link>
+        <Link className="dropdown-item" to="Africa" onClick={()=>onClickFilterRegion("Africa")} >Africa</Link>
+        <Link className="dropdown-item" to="/Americas" onClick={()=>onClickFilterRegion("Americas")}>America</Link>
+        <Link className="dropdown-item" to="/Asia" onClick={()=>onClickFilterRegion("Asia")}>Asia</Link>
+        <Link className="dropdown-item" to="/Europe" onClick={()=>onClickFilterRegion("Europe")}>Europe</Link>
+        <Link className="dropdown-item" to="/Oceania" onClick={()=>onClickFilterRegion("Oceania")}>Oceania</Link>
         </div>
     </div>
 </div>
