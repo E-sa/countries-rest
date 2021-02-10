cff36e16b3d731e877ee1fa9e76dc3feae6e68a9
E-sa <35602280+E-sa@users.noreply.github.com>
2021-02-10 12:49:44 +0330

    Update SearchInput.js

--- src/component/SearchInput.js
+++ src/component/SearchInput.js
@@ -1,37 +1,19 @@
 //search input is here
-//type anything and it send what u have typed to App.js 
-//and from there and inside axios the results will be filtered 
+//type anything and it send what u have typed to App.js
 
+import React from "react";
+import "../style/style.scss";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import '../style/style.scss'
-
-
-class SearchInput extends React.Component {
-
-    state={
-        searchTerm:''
-    }
-
-    editSearchTerm = (event) => {
-
-        this.setState({searchTerm:event.target.value})
-        this.props.parentCallback(event.target.value);
-        event.preventDefault();
-    }
-
-    render(){
-        return(
-            <div id="search-input">
-                <label>
-                 <input type="text" value={this.state.searchTerm} placeholder="Search for a country... " onChange={this.editSearchTerm} />
-                </label>
-            </div>
-        )
-    }
+export default function SearchInput({ onSearch }) {
+  return (
+    <div id="search-input">
+      <label>
+        <input
+          type="text"
+          placeholder="Search for a country... "
+          onChange={(e) => onSearch(e.target.value)}
+        />
+      </label>
+    </div>
+  );
 }
-
-
-
-export default SearchInput;
